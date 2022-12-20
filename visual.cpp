b2c9413cc5139f3413a119566dad5622bcaea3ad
MD-Ragib-Asif <84400635+MD-Ragib-Asif@users.noreply.github.com>
2022-12-21 02:40:33 +0600

    text

---
+++ visual.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+
+//     cout <<"Welcome to this video";
+//     return 0;
+
+// }
+
+
+//----------------------------------------------------------------------------
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1;
+    int n2;
+    int sum=0;
+    cout <<"Enter the first number:"<< endl;
+    cin >> n1;
+    cout <<"Enter the second number:"<< endl;
+    cin >> n2;
+    sum=n1+n2;
+    cout << sum;
+
+}
